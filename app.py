6882ad26e75c07f7c48f9921a432e778556ce8c9
lilmonkey912 <chloeeezheng@gmail.com>
2025-06-21 20:32:39 -0400

    ML Flask Server (#4)
    * add working flask server + deep face facial expression detection + yolo phone detection
    
    * verified phone detection function + changed image input to binary image upload
    
    * Add sample output image from YOLO detection
    
    * Create README.md
    
    * Update README.md
    
    * Gaze Tracking returns eye closed or eye centre
    
    * Track large model file with Git LFS
    
    * temp: save test output before switching branch
    
    * finalize flask server integration
    
    * cleanup

---
+++ app.py
@@ -0,0 +1,73 @@
+#Python3 -m venv env
+#source env/bin/activate
+#pip install flask flask-cors deepface ultralytics opencv-python
+#export FLASK_APP=app
+# flask run
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import base64
+import numpy as np
+import cv2
+import json
+from deepface import DeepFace
+from ultralytics import YOLO
+from gazetracking.gaze_tracking import GazeTracking
+
+
+app = Flask(__name__)
+CORS(app)
+
+#yolov11 nano
+yolo_model = YOLO("yolov8n.pt")
+
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    try:
+        # read binary  image
+        img_bytes = request.data
+        nparr = np.frombuffer(img_bytes, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        # phone detection with YOLO
+        results = yolo_model(img)
+        has_phone = False
+        for box in results[0].boxes:
+            cls_id = int(box.cls[0])
+            label = results[0].names[cls_id]
+            print (label)
+            if label in ['cell phone', 'remote', 'teddy bear', 'donut', 'toilet']:
+                has_phone = True
+                break
+            # Draw boxes on the image
+            annotated_frame = results[0].plot()
+            cv2.imwrite("output.jpg", annotated_frame)  # Optional: Save to disk
+          #  cv2.imshow("YOLO Detection", annotated_frame)
+          #  cv2.waitKey(0)
+          #  cv2.destroyAllWindows()
+
+        # facial expression detection with DeepFace
+        face_result = DeepFace.analyze(img, actions=['emotion'], enforce_detection=False)
+        emotion = face_result[0]['dominant_emotion']
+
+        # eye tracking feature with GazeTracking
+        gaze = GazeTracking()
+        gaze.refresh(img)
+        eye_centre = gaze.is_center()
+        eye_closed = gaze.is_blinking()
+        eye_centre = False
+        if not eye_closed:
+            eye_centre = gaze.is_center()
+        # Return Json results
+        return jsonify({
+            "has_phone": has_phone,
+            "emotion": emotion,
+            "eye_centre": eye_centre,
+            "eye_closed": eye_closed
+        })
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
---
+++ test.py
@@ -0,0 +1,25 @@
+#from deepface import DeepFace
+
+#result = DeepFace.analyze(
+   # img_path = "https://raw.githubusercontent.com/serengil/deepface/master/tests/dataset/img1.jpg",
+ #   img_path = "crying.jpeg",
+  #  actions = ["emotion"],
+   # enforce_detection=False
+#)
+
+#print("Result:", result[0]["dominant_emotion"])
+
+import requests
+import json
+
+# Base64 encoded
+with open("chlophone.jpeg", "rb") as image_file:
+    img_bytes = image_file.read()
+
+# send POST request to flask server
+response = requests.post("http://127.0.0.1:5000/analyze", 
+                         data=img_bytes, 
+                         headers={"Content-Type": "image/jpeg"})
+
+# print result
+print(response.json())
